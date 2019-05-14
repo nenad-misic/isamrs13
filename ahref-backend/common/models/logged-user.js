97be4b323fd2be1e178a00170555ea21ae38b327
Nenad Misic <nenad.misic997@gmail.com>
2019-05-14 20:19:56 +0200

    Changed room and flight models

--- ahref-backend/common/models/logged-user.js
+++ ahref-backend/common/models/logged-user.js
@@ -2,7 +2,8 @@
 var config = require('../../server/config.json');
 var path = require('path');
 
-var flag = true;
+var flagCar = true;
+var flagRoom = true;
 
 module.exports = function(Loggeduser) {
   Loggeduser.afterRemote('**', function(ctx, modelInstance, next)  {
@@ -12,16 +13,26 @@ module.exports = function(Loggeduser) {
 
   Loggeduser.beforeRemote('*.__create__mCarReservations', function(ctx,model,next) {
     console.log('here');
-    flag = true;
+    flagCar = true;
     var Mcarreservation = Loggeduser.app.models.MCarReservation;
-    doReservation(Mcarreservation, ctx, model, next, function(e) {
-      flag = false;
+    doCarReservation(Mcarreservation, ctx, model, next, function(e) {
+      flagCar = false;
+      next(e);
+    });
+  });
+
+  Loggeduser.beforeRemote('*.__create__mRoomReservations', function(ctx,model,next) {
+    console.log('here');
+    flagRoom = true;
+    var Mroomreservation = Loggeduser.app.models.MRoomReservation;
+    doRoomReservation(Mroomreservation, ctx, model, next, function(e) {
+      flagRoom = false;
       next(e);
     });
   });
 
   
-function doReservation(Mcarreservation, ctx, model, next, errorCallback) {
+function doCarReservation(Mcarreservation, ctx, model, next, errorCallback) {
     // models
     var sqlCarReservation = Mcarreservation.app.models.CarReservation;
     var sCar = Mcarreservation.app.models.sCar;
@@ -42,7 +53,7 @@ function doReservation(Mcarreservation, ctx, model, next, errorCallback) {
                 where: {sCarId: car.id},
               }).then((data)=> {
                 data.forEach((element) => {
-                  if (flag) {
+                  if (flagCar) {
                     var start1 = element.startDate.getTime();
                     var end1 = element.endDate.getTime();
                     var start2 = ctx.req.body.startDate;
@@ -50,25 +61,25 @@ function doReservation(Mcarreservation, ctx, model, next, errorCallback) {
                     if ((start1 >= start2 && start1 <= end2) ||
                       (start2 >= start1 && start2 <= end1)) {
                       tx.rollback(function(err) {
-                        if (err && flag) errorCallback(err);
+                        if (err && flagCar) errorCallback(err);
   
                         var error = new Error('Car is already reserved');
                         error.statusCode = error.status = 404;
-                        if (flag)
+                        if (flagCar)
                           errorCallback(error);
                       });
                     }
                   }
                 });
   
-                if (err && flag) {
+                if (err && flagCar) {
                   tx.rollback(function(err) {
-                    if (err && flag) errorCallback(err);
+                    if (err && flagCar) errorCallback(err);
                   });
                   errorCallback(err);
                 }
                 // fetch car from sql
-                if (flag) {
+                if (flagCar) {
                   sCar.findOne({where: {mongoId: ctx.req.body.carId}})
                     .then((car)=>{
                       // create reservation
@@ -81,16 +92,16 @@ function doReservation(Mcarreservation, ctx, model, next, errorCallback) {
                         },
                         {transaction: tx},
                         function(err, cr) {
-                          if (err && flag) {
+                          if (err && flagCar) {
                             tx.rollback(function(err) {
-                              if (err && flag)  errorCallback(err);
+                              if (err && flagCar)  errorCallback(err);
                             });
                             errorCallback(err);
                           }
                           // commit and end before-hook
-                          if (flag) {
+                          if (flagCar) {
                             tx.commit(function(err) {
-                              if (err && flag)  errorCallback(err);
+                              if (err && flagCar)  errorCallback(err);
                               next();
                             });
                           }
@@ -102,6 +113,99 @@ function doReservation(Mcarreservation, ctx, model, next, errorCallback) {
         });
     });
   }
+
+
+  function doRoomReservation(Mroomreservation, ctx, model, next, errorCallback) {
+    // models
+    var sqlRoomReservation = Mroomreservation.app.models.RoomReservation;
+    var sRoom = Mroomreservation.app.models.sRoom;
+    // data source
+    var postgres = sRoom.app.dataSources.postgres;
+    // begin transaction
+    sqlRoomReservation.beginTransaction({
+        isolationLevel: sqlRoomReservation.Transaction.READ_COMMITTED,
+    }, function(err, tx) {
+        if (err) errorCallback(err);
+        // lock room for update
+        postgres.connector.execute(
+            'SELECT * FROM sRoom WHERE mongoId = $1 FOR UPDATE;',
+            [ctx.req.body.roomId], function(err, data) {
+                sRoom.findOne({where: {mongoId: ctx.req.body.roomId}}).then((room) => {
+                    sqlRoomReservation.find({where: {sRoomId: room.id}}).then((data) => {
+                        var foundOne = false;
+                        data.forEach((element) => {
+                            if (flagRoom) {
+                                // check if room is available during time period
+                                console.log(element);
+                                var start1 = element.startDate.getTime();
+                                var end1 = element.endDate.getTime();
+                                var start2 = new Date(ctx.req.body.startDate).getTime();
+                                var end2 = new Date(ctx.req.body.endDate).getTime();
+                                console.log({
+                                    start1: start1,
+                                    end1: end1,
+                                    start2: start2,
+                                    end2: end2
+                                });
+                                if ((start1 >= start2 && start1 <= end2) ||
+                                    (start2 >= start1 && start2 <= end1)) {
+                                    foundOne = true;
+                                }
+                            }
+                        })
+                        if (foundOne) {
+                            tx.rollback(function(err) {
+                                if (err && flagRoom) errorCallback(err);
+
+                                var error = new Error('Room is not available');
+                                error.statusCode = error.status = 404;
+                                if (flagRoom) errorCallback(error);
+                            });
+                        }
+
+                        if (err && flagRoom) {
+                            tx.rollback(function(err) {
+                              if (err && flagRoom) errorCallback(err);
+                            });
+                            errorCallback(err);
+                        }
+
+                        if (flagRoom) {
+                            sRoom.findOne({where: {mongoId: ctx.req.body.roomId}}).then((room)=>{
+                                // create reservation
+                                sqlRoomReservation.create(
+                                  {
+                                    timeStamp: ctx.req.body.timeStamp,
+                                    sRoom: room,
+                                    startDate: ctx.req.body.startDate,
+                                    endDate: ctx.req.body.endDate,
+                                  },
+                                  {transaction: tx},
+                                  function(err, rr) {
+                                    if (err && flagRoom) {
+                                      tx.rollback(function(err) {
+                                        if (err && flagRoom)  errorCallback(err);
+                                      });
+                                      errorCallback(err);
+                                    }
+                                    // commit and end before-hook
+                                    if (flagRoom) {
+                                      tx.commit(function(err) {
+                                        if (err && flagRoom)  errorCallback(err);
+                                        next();
+                                      });
+                                    }
+                                  });
+                              });
+                          }
+
+                    })
+                })
+            }
+        )
+    })
+}
+
   
     Loggeduser.afterRemote('create', function(ctx, modelInstance, next) {
         var Role = Loggeduser.app.models.Role;
