627de2144f822926dfe5f9c73744c8997a6a5622
Nenad Misic <nenad.misic997@gmail.com>
2019-06-18 16:40:05 +0200

    Added pessimistic locking for quick car reservations

--- ahref-backend/common/models/m-car-reservation.js
+++ ahref-backend/common/models/m-car-reservation.js
@@ -103,10 +103,8 @@ function doReservation(Mcarreservation, ctx, model, next, errorCallback) {
                         // commit and end before-hook
                         if (flag) {
                           tx.commit(function(err) {
-                            
-                            console.log('100');
                             if (err && flag)  errorCallback(err);
-                            next();
+                            calculateCarPrice(Mcarreservation, ctx, next)
                           });
                         }
                       });
@@ -117,22 +115,32 @@ function doReservation(Mcarreservation, ctx, model, next, errorCallback) {
       });
   });
 }
-
-function calculatePrice(Mcarreservation, ctx) {
-  var models = Mcarreservation.app.models;
+function calculateCarPrice(Loggeduser, ctx, callback) {
+  var models = Loggeduser.app.models;
   var start = ctx.req.body.startDate;
   var end = ctx.req.body.endDate;
   var days = (end - start)/(24*60*60*1000)
 
   models.Car.findOne({where: {id: ctx.req.body.carId}}).then((car) => {
-    models.RACService.fondOne({where: {id: car.rACServiceId}}).then((racService) => {
-      models.RPriceListItem.find({where: {rPriceListId: racService.priceListId}}).then((items) => {
-        items.forEach((item) => {
-          if (item.price != 0) {
-            if (item.carType === car.carType) {
-              return item.price * days;
-            }
+    //console.log(car);
+    models.RACService.findOne({where: {id: car.rACServiceId}}).then((racService) => {
+      //console.log(racService);
+      models.RPriceList.findOne({where: {id: racService.rPriceListId}}).then((priceList) => {
+        models.RPriceListItem.find({where: {rPriceListId: priceList.id}}).then((items) => {
+          if (items == []) {
+            var error = new Error('Car has no price defined');
+            error.statusCode = error.status = 404;
+            callback(error);
           }
+          items.forEach((item) => {
+            if (item.price != 0) {
+              if (item.carType === car.carType) {
+                item.price *= 0.8;
+                ctx.req.body.price = item.price * days;
+                callback();
+              }
+            }
+          })
         })
       })
     })
--- ahref-frontend/src/app/quick-car-section-view/quick-car-section-view.component.ts
+++ ahref-frontend/src/app/quick-car-section-view/quick-car-section-view.component.ts
@@ -1,10 +1,12 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {QuickCarReservation, RACService} from '../shared/sdk/models';
+import {CombinedReservation, QuickCarReservation, RACService} from '../shared/sdk/models';
 import {LoggedUserApi, MCarReservationApi, RACServiceApi} from '../shared/sdk/services/custom';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {Location} from '@angular/common';
 import {LoopBackConfig} from '../shared/sdk';
 import {API_VERSION} from '../shared/baseurl';
+import {CombinedService} from '../services/combined.service';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-quick-car-section-view',
@@ -14,18 +16,29 @@ import {API_VERSION} from '../shared/baseurl';
 export class QuickCarSectionViewComponent implements OnInit {
 
   qcres: QuickCarReservation[];
+  combinedReservation: CombinedReservation;
   racid: string;
   constructor(private racapi: RACServiceApi,
               private mCarResApi: MCarReservationApi,
-              private route: ActivatedRoute,
               private luapi: LoggedUserApi,
               private location: Location,
+              private route: ActivatedRoute,
+              private toastr: ToastrService,
+              private router: Router,
+              private combinedService: CombinedService,
               @Inject('baseURL') private baseURL) {
     LoopBackConfig.setBaseURL(baseURL);
     LoopBackConfig.setApiVersion(API_VERSION);
   }
 
   ngOnInit() {
+    this.combinedService.combinedReservation.subscribe((cr) => {
+      if (cr.id) {
+        this.combinedReservation = cr;
+      } else {
+        this.combinedReservation = null;
+      }
+    });
     this.racid = this.route.snapshot.params['id'];
     this.racapi.findById(this.racid, {include: {cars: 'quickCarReservations'}}).subscribe((rac: RACService) => {
       this.qcres = [];
@@ -37,11 +50,15 @@ export class QuickCarSectionViewComponent implements OnInit {
     });
   }
 
-  reserve(qcr) {
-    /*
-    this.luapi.bindQuick(this.luapi.getCachedCurrent().id, qcr.id, qcr.mCarReservationId).subscribe((res) => {
-      alert('QUICK CAR RESURVATION RESURVATED PADRE!' + res.retval);
-    });*/
+  reserve(qcr: QuickCarReservation) {
+    this.luapi.bindQuick(this.luapi.getCachedCurrent().id, qcr.id, qcr.mCarReservationId, qcr.carId ,this.combinedReservation.id)
+      .subscribe((res) => {
+      this.toastr.success('Reservation successful');
+      this.combinedService.refreshCombinedReservation();
+      this.router.navigate(['/flow']);
+    }, (err) => {
+      this.toastr.success(err.message, 'Reservation failed');
+    });
   }
 
 }
